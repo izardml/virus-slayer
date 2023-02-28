e89ad840c7beb32d229b77561a5087c937d53245
Nizar Daffa <nizartokuzet@gmail.com>
2023-02-28 16:36:33 +0700

    Remake

--- script.js
+++ script.js
@@ -1,43 +1,63 @@
-// 5 80 5 80 5 80 5 80 5
+function show() {
+    let input = document.getElementById('name')
+    let playBtn = document.getElementById('playBtn')
 
-var centerLine, line1, line2, line3, line4
-var dNote, fNote, jNote, kNote
-var dBtn, fBtn, jBtn, kBtn
+    if(input.value != '') {
+        playBtn.style.display = 'block'
+    } else {
+        playBtn.style.display = 'none'
+    }
+}
+
+// ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## 
+
+var
+
+dLane,
+fLane,
+jLane,
+kLane,
+
+dVirus = [],
+fVirus = [],
+jVirus = [],
+kVirus = [],
+
+dangerArea,
+border,
+dfjkKey,
+failArea
 
 function startGame() {
     myGameArea.start()
 
-    centerLine = new component(10, 720, 'skyblue', 635, 0)
+    dLane = new component(60, 600, 'gray', 345, 0)
+    fLane = new component(60, 600, 'gray', 415, 0)
+    jLane = new component(60, 600, 'gray', 485, 0)
+    kLane = new component(60, 600, 'gray', 555, 0)
 
-    line1 = new component(10, 720, 'skyblue', 455, 0)
-    line2 = new component(10, 720, 'skyblue', 545, 0)
-    line3 = new component(10, 720, 'skyblue', 725, 0)
-    line4 = new component(10, 720, 'skyblue', 815, 0)
-
-    dNote = new component(50, 50, 'red', 480, 0)
-    fNote = new component(50, 50, 'red', 570, 0)
-    jNote = new component(50, 50, 'red', 660, 0)
-    kNote = new component(50, 50, 'red', 750, 0)
-
-    dBtn = new component(80, 150, 'rgba(255, 255, 255, 0.5)', 465, 570)
-    fBtn = new component(80, 150, 'rgba(255, 255, 255, 0.5)', 555, 570)
-    jBtn = new component(80, 150, 'rgba(255, 255, 255, 0.5)', 645, 570)
-    kBtn = new component(80, 150, 'rgba(255, 255, 255, 0.5)', 735, 570)
+    dangerArea = new component(270, 50, 'rgba(255, 0, 0, 0.5)', 345, 440)
+    border = new component(270, 10, 'black', 345, 490)
+    dfjkKey = new component(270, 100, 'rgba(255, 255, 255, 0.5)', 345, 500)
+    failArea = new component(270, 20, 'lightgray', 345, 550)
 }
 
 var myGameArea = {
     canvas: document.createElement('canvas'),
     start: function() {
-        this.canvas.width = 1280
-        this.canvas.height = 720
+        document.getElementById('start').style.display = 'none'
+        this.canvas.width = 960 // 480
+        this.canvas.height = 600
         this.context = this.canvas.getContext('2d')
         document.body.insertBefore(this.canvas, document.body.childNodes[0])
-        this.interval = setInterval(updateGameArea, 10)
+        this.frameNo = 0
+        this.interval = setInterval(updateGameArea, 20)
         window.addEventListener('keydown', function(e) {
-            myGameArea.key = e.keyCode
+            myGameArea.keys = (myGameArea.keys || [])
+            myGameArea.keys[e.keyCode] = true
         })
         window.addEventListener('keyup', function(e) {
-            myGameArea.key = false
+            myGameArea.keys[e.keyCode] = false
         })
     },
     clear: function() {
@@ -75,35 +95,110 @@ function component(width, height, color, x, y) {
     }
 }
 
+var score = 0
+var fail = 0
+
 function updateGameArea() {
-    if((dNote.crashWith(dBtn)) || (fNote.crashWith(fBtn)) || (jNote.crashWith(jBtn)) || (kNote.crashWith(kBtn))) {
+    for(i = 0; i < dVirus.length; i++) {
+        if(dVirus[i].crashWith(dangerArea) && (myGameArea.keys && myGameArea.keys[68])) {
+            dVirus[i].y += myGameArea.canvas.height
+            score++
+        }
+
+        if(dVirus[i].crashWith(failArea)) {
+            dVirus[i].y += myGameArea.canvas.height
+            fail++
+        }
+    }
+    for(i = 0; i < fVirus.length; i++) {
+        if(fVirus[i].crashWith(dangerArea) && (myGameArea.keys && myGameArea.keys[70])) {
+            fVirus[i].y += myGameArea.canvas.height
+            score++
+        }
+
+        if(fVirus[i].crashWith(failArea)) {
+            fVirus[i].y += myGameArea.canvas.height
+            fail++
+        }
+    }
+    for(i = 0; i < jVirus.length; i++) {
+        if(jVirus[i].crashWith(dangerArea) && (myGameArea.keys && myGameArea.keys[74])) {
+            jVirus[i].y += myGameArea.canvas.height
+            score++
+        }
+
+        if(jVirus[i].crashWith(failArea)) {
+            jVirus[i].y += myGameArea.canvas.height
+            fail++
+        }
+    }
+    for(i = 0; i < kVirus.length; i++) {
+        if(kVirus[i].crashWith(dangerArea) && (myGameArea.keys && myGameArea.keys[75])) {
+            kVirus[i].y += myGameArea.canvas.height
+            score++
+        }
+
+        if(kVirus[i].crashWith(failArea)) {
+            kVirus[i].y += myGameArea.canvas.height
+            fail++
+        }
+    }
+
+    if(fail >= 5) {
         myGameArea.stop()
     }
+
     myGameArea.clear()
+    myGameArea.frameNo++
 
-    centerLine.update()
-    line1.update()
-    line2.update()
-    line3.update()
-    line4.update()
+    dLane.update()
+    fLane.update()
+    jLane.update()
+    kLane.update()
 
-    dNote.y++
-    fNote.y++
-    jNote.y++
-    kNote.y++
+    dangerArea.update()
+    failArea.update()
 
-    if(myGameArea.key && myGameArea.key == 68) { dNote.y += myGameArea.canvas.height }
-    if(myGameArea.key && myGameArea.key == 70) { fNote.y += myGameArea.canvas.height }
-    if(myGameArea.key && myGameArea.key == 74) { jNote.y += myGameArea.canvas.height }
-    if(myGameArea.key && myGameArea.key == 75) { kNote.y += myGameArea.canvas.height }
+    if(myGameArea.frameNo == 1 || everyinterval(10)) {
+        var random = Math.floor(Math.random() * 4)
 
-    dNote.update()
-    fNote.update()
-    jNote.update()
-    kNote.update()
+        if(random == 0) {
+            dVirus.push(new component(50, 50, 'red', 350, -50))
+        } else if(random == 1) {
+            fVirus.push(new component(50, 50, 'red', 420, -50))
+        } else if(random == 2) {
+            jVirus.push(new component(50, 50, 'red', 490, -50))
+        } else if(random == 3) {
+            kVirus.push(new component(50, 50, 'red', 560, -50))
+        }
+    }
 
-    dBtn.update()
-    fBtn.update()
-    jBtn.update()
-    kBtn.update()
+    var z = 5
+
+    for(i = 0; i < dVirus.length; i++) {
+        dVirus[i].y += z
+        dVirus[i].update()
+    }
+    for(i = 0; i < fVirus.length; i++) {
+        fVirus[i].y += z
+        fVirus[i].update()
+    }
+    for(i = 0; i < jVirus.length; i++) {
+        jVirus[i].y += z
+        jVirus[i].update()
+    }
+    for(i = 0; i < kVirus.length; i++) {
+        kVirus[i].y += z
+        kVirus[i].update()
+    }
+
+    border.update()
+    dfjkKey.update()
+}
+
+function everyinterval(n) {
+    if((myGameArea.frameNo / n) % 1 == 0) {
+        return true
+    }
+    return false
 }
